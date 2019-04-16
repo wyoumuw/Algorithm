c9fe6c675120c0acd5817dec45b09ae110f4bddf
wyoumuw <wyoumuw@gmail.com>
2019-04-16 09:24:20 +0800

    归并排序

--- src/main/java/com/youmu/maven/Algorithm/sort/MergeSort.java
+++ src/main/java/com/youmu/maven/Algorithm/sort/MergeSort.java
@@ -37,10 +37,27 @@ public class MergeSort implements Sortable {
             if (arr[li] > arr[ri]) {
                 tmp = arr[li];
                 arr[li] = arr[ri];
-				arr[ri] = tmp;
+                arr[ri] = tmp;
 
             }
-			li++;
+            li++;
         }
     }
+
+    /**
+     * 把数组的连续的两个子数组进行归并
+     * @param arr 主数组
+     * @param start 子数组1的头(包含)
+     * @param mid 子数组1的尾(不包含),子数组2的头(包含)
+     * @param end 子数组2的尾(不包含)
+     */
+    private void merge(int[] arr, int start, int mid, int end) {
+        int i1 = start, i2 = mid, len1 = mid - start, len2 = end - mid;
+        while (i1 < len1 && i2 < len2) {
+            if (arr[i1] < arr[i2]) {
+
+            }
+        }
+    }
+
 }
